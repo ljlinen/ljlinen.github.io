dfe7eab4e6a1da663cadaa06edb29fd86f764664
LJLinen <SiphoMoloto0@gmail.com>
2024-05-24 06:30:58 +0200

    first commit to my first portfolio website

---
+++ js/anim.js
@@ -0,0 +1,20 @@
+const sidenav = document.querySelector('.side-nav')
+const menu = document.querySelector('.menu')
+const menuline = menu.nextElementSibling.firstElementChild;
+
+let open = false;
+menu.addEventListener('click', () => {
+    if(!open) {
+        menu.setAttribute('src','../src/icon/cross.svg')
+        menu.style.transform = 'rotateZ(180deg)'
+        menuline.style.transform = 'translateX(-100%)'
+        sidenav.style.transform = 'translateX(0)'
+        open = !open;
+    } else {
+        menu.setAttribute('src','../src/icon/menu.svg')
+        sidenav.style.transform = 'translateX(150%)'
+        menuline.style.transform = 'translateX(100%)'
+        menu.style.transform = 'rotateZ(0deg)'
+        open = !open;
+    }
+})
