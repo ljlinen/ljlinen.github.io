04faa485dcf0d607d3b6faecfa1a4fa509b28505
LJLinen <SiphoMoloto0@gmail.com>
2025-03-11 00:20:37 +0200

    Added animations for buttons hover for the sidenav buttons and close icon.

--- src/js/component/Nav.jsx
+++ src/js/component/Nav.jsx
@@ -3,25 +3,35 @@ import iconCross from '../../asset/icon/cross.svg'
 import imgProfile from '../../asset/img/profile.jpeg'
 import { scrollToElement } from '../global/reusable'
 import '../../css/component/nav.css'
+import { useEffect, useState } from 'react'
 export default function Nav({isNavOpen, setOpen}) {
+
+  const [focusProfile, setFocusProfile] = useState()
 
   const handleSideNavItemCick = (classTocrollTo) => {
     scrollToElement(classTocrollTo);
     setOpen(!open);
   }
 
+  useEffect(() => {
+    console.log(focusProfile);
+  }, [focusProfile])
+
   return (
     <>
       <div className={isNavOpen ? "side-nav" : 'side-nav side-nav-open'}>
         <div className="side-nav-inner" style={{transform: isNavOpen ? 'translateX(0)' : 'translateX(150%)'}}>
                 <img className="menu i" src={iconCross} alt="Menu-Cancel" onClick={() => setOpen(!isNavOpen)} />
-          <div className="profile">
-            <img src={imgProfile} alt="profile image" />
+          <div className="profile-main">
+            <div className={focusProfile ? "profile profile-focus" : "profile"} onFocus={() => setFocusProfile(true)} onBlur={() => setFocusProfile(true)}>
+              <img src={imgProfile} alt="profile image" />
+            </div>            
           </div>
+
           <ul>
-            <li onClick={() => handleSideNavItemCick('.s1-sec-work')}>my work</li>
-            <li onClick={() => handleSideNavItemCick('.about-me')}>about me</li>
-            <li onClick={() => handleSideNavItemCick('.footer')}>connect</li>
+            <li onClick={() => handleSideNavItemCick('.s1-sec-work')}><span></span>my work<span></span></li>
+            <li onClick={() => handleSideNavItemCick('.about-me')}><span></span>about me<span></span></li>
+            <li onClick={() => handleSideNavItemCick('.footer')}><span></span>connect<span></span></li>
             <div className="wrap"></div>
           </ul>
         </div>
